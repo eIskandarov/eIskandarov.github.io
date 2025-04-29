282def88f568cca4d7cf3efd5b29d2d862f5a12a
eiskandarov <eiskandarov@gmail.com>
2025-04-30 00:36:57 +0500

    feat: update web worker

--- preventer.js
+++ preventer.js
@@ -2,6 +2,7 @@ document.addEventListener(
   "dblclick",
   function(e) {
     e.preventDefault();
+    console.log("prevented");
   },
   { passive: false },
 );
--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,59 @@
-const cacheName = 'worker-cache-v1.3';
+const cacheName = "worker-cache-v1.4";
 const assets = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './images/favicon/android-chrome-192x192.png',
-  './images/dice-1.png',
-  './images/dice-2.png',
-  './images/dice-3.png',
-  './images/dice-4.png',
-  './images/dice-5.png',
-  './images/dice-6.png',
-  './sounds/d1/d1_01.wav',
-  './sounds/d1/d1_02.wav',
-  './sounds/d1/d1_03.wav',
-  './sounds/d1/d1_04.wav',
-  './sounds/d1/d1_05.wav',
-  './sounds/d1/d1_06.wav',
-  './sounds/dice-roll/dice-roll_01.wav',
-  './sounds/dice-roll/dice-roll_02.wav',
-  './sounds/dice-roll/dice-roll_03.wav',
-  './sounds/hold/hold_01.wav',
-  './sounds/hold/hold_02.wav',
-  './sounds/hold/hold_03.wav',
-  './sounds/new-game/new-game_01.wav',
-  './sounds/new-game/new-game_02.wav',
-  './sounds/new-game/new-game_03.wav',
-  './sounds/winner/winner_01.wav',
-  './sounds/winner/winner_02.wav',
-  './sounds/winner/winner_03.wav',
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./images/favicon/android-chrome-192x192.png",
+  "./images/dice-1.png",
+  "./images/dice-2.png",
+  "./images/dice-3.png",
+  "./images/dice-4.png",
+  "./images/dice-5.png",
+  "./images/dice-6.png",
+  "./sounds/d1/d1_01.wav",
+  "./sounds/d1/d1_02.wav",
+  "./sounds/d1/d1_03.wav",
+  "./sounds/d1/d1_04.wav",
+  "./sounds/d1/d1_05.wav",
+  "./sounds/d1/d1_06.wav",
+  "./sounds/dice-roll/dice-roll_01.wav",
+  "./sounds/dice-roll/dice-roll_02.wav",
+  "./sounds/dice-roll/dice-roll_03.wav",
+  "./sounds/hold/hold_01.wav",
+  "./sounds/hold/hold_02.wav",
+  "./sounds/hold/hold_03.wav",
+  "./sounds/new-game/new-game_01.wav",
+  "./sounds/new-game/new-game_02.wav",
+  "./sounds/new-game/new-game_03.wav",
+  "./sounds/winner/winner_01.wav",
+  "./sounds/winner/winner_02.wav",
+  "./sounds/winner/winner_03.wav",
 ];
 
 // console.log('Service Worker scope:', self.location);
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => {
+    caches.open(cacheName).then((cache) => {
       return cache.addAll(assets);
-    })
+    }),
   );
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
     event.respondWith(
       caches
-        .match('./index.html')
-        .then(cached => cached || fetch(event.request))
+        .match("./index.html")
+        .then((cached) => cached || fetch(event.request)),
     );
     return;
   }
 
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
-    })
+    }),
   );
 });
